c3d8d205c5867cfc529b4101053e6a9fea5b634b
Rupa <rupa211984@gmail.com>
2019-11-11 11:37:54 -0800

    Pass tests

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,85 @@
 require_relative "../config/environment.rb"
 
 class Student
+  attr_accessor :id, :name, :grade
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def initialize(name, grade, id = nil)
+    @id = id
+    @name = name
+    @grade = grade
+  end
 
+  def save
+    @id.nil? ? persist : update
+  end
+
+  def persist
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, @name, @grade)
+
+    sql = <<-SQL
+      SELECT id 
+      FROM students 
+      ORDER BY id DESC 
+      LIMIT 1
+    SQL
+
+    @id = DB[:conn].execute(sql)[0][0]
+  end
+
+  def update
+    sql = <<-SQL
+      UPDATE students 
+      SET name = ?, grade = ?
+      WHERE id = ?;
+    SQL
+
+    DB[:conn].execute(sql, @name, @grade, @id)
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+    SQL
+
+    row = DB[:conn].execute(sql, name)[0]
+    new_from_db(row)
+  end
+
+  def self.new_from_db(row)
+    student = Student.new(row[1], row[2], row[0])
+  end
+
+  def self.create(name, grade)
+    student = self.new(name, grade)
+    student.save
+    student
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+    SQL
+
+    DB[:conn].execute(sql)
+  end
 
 end
